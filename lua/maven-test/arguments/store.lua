24e9a15952c8e9af8dfa00e5e7fc3c92b4c26a1d
JF Lavoie <jflavoie.perso@gmail.com>
2026-03-19 21:43:16 -0400

    merge transient stores

---
+++ lua/maven-test/arguments/store.lua
@@ -0,0 +1,39 @@
+--- Maven custom arguments store module
+--- Provides a singleton key-value store for managing custom Maven arguments
+--- Each argument is stored with its text as the key and an Argument object as the value
+--- Persists to arguments.json in the project data directory
+---
+--- The store manages Maven arguments that can be:
+--- - Added with active/inactive state
+--- - Toggled on/off
+--- - Retrieved for appending to Maven commands
+--- - Persisted across Neovim sessions
+---
+--- @module 'maven-test.arguments.store'
+--- @see maven-test.arguments.argument For Argument class definition
+--- @see maven-test.store.key_value_store For underlying storage implementation
+---
+--- @usage
+---   local arguments_store = require("maven-test.arguments.store")
+---   local Argument = require("maven-test.arguments.argument").Argument
+---
+---   -- Add a new argument
+---   arguments_store:add("-X", Argument.new("-X", true))
+---
+---   -- Get an argument
+---   local arg = arguments_store:get("-X")
+---
+---   -- List all arguments
+---   local all_args = arguments_store:list()
+
+local Argument = require("maven-test.arguments.argument").Argument
+local KeyValueStore = require("maven-test.store.key_value_store").KeyValueStore
+
+--- Singleton instance of KeyValueStore for Maven custom arguments
+--- Persists to arguments.json in ~/.local/share/nvim/maven.nvim.test/<project-name>/
+--- @type KeyValueStore
+local store = KeyValueStore.new("arguments.json", function(data)
+	return Argument.new(data.text, data.active)
+end)
+
+return store
