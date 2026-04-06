a9957509bfcfedd20e8e2e27e963ed3f2cd528ac
JF Lavoie <jflavoie.perso@gmail.com>
2026-04-06 11:22:58 -0400

    remove dead code

--- lua/maven-test/user_commands.lua
+++ lua/maven-test/user_commands.lua
@@ -17,7 +17,6 @@ local M = {}
 M.register_commands = function(projectConfigs)
 	for _, projectConfig in ipairs(projectConfigs) do
 		local pattern = projectConfig.pattern
-		vim.print("jf-debug-> 'pattern': " .. tostring(pattern))
 		-- Register FileType autocmd to create buffer-local commands for Java files
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = pattern,
@@ -66,66 +65,3 @@ M.register_commands = function(projectConfigs)
 end
 
 return M
-
--- -- Register FileType autocmd to create buffer-local commands for Java files
--- vim.api.nvim_create_autocmd("FileType", {
--- 	pattern = "java",
--- 	group = vim.api.nvim_create_augroup("MavenTestJava", { clear = true }),
--- 	callback = function(args)
--- 		-- Test execution commands
--- 		vim.api.nvim_buf_create_user_command(args.buf, "MavenTest", function()
--- 			ensure_loaded()
--- 			require("maven-test.functions").run_test()
--- 		end, { desc = "Test method picker" })
---
--- 		vim.api.nvim_buf_create_user_command(args.buf, "MavenTestClass", function()
--- 			ensure_loaded()
--- 			require("maven-test.functions").run_test_class()
--- 		end, { desc = "Run all tests in the current Java class" })
---
--- 		vim.api.nvim_buf_create_user_command(args.buf, "MavenTestAll", function()
--- 			ensure_loaded()
--- 			require("maven-test.functions").run_all_tests()
--- 		end, { desc = "Run all tests in the current Java class" })
---
--- 		-- Debug mode commands
--- 		vim.api.nvim_buf_create_user_command(args.buf, "MavenTestDebug", function()
--- 			ensure_loaded()
--- 			require("maven-test.functions").run_test_debug()
--- 		end, { desc = "Test method picker (debug mode)" })
---
--- 		vim.api.nvim_buf_create_user_command(args.buf, "MavenTestClassDebug", function()
--- 			ensure_loaded()
--- 			require("maven-test.functions").run_test_class_debug()
--- 		end, { desc = "Run all tests in the current Java class (debug mode)" })
---
--- 		vim.api.nvim_buf_create_user_command(args.buf, "MavenTestAllDebug", function()
--- 			ensure_loaded()
--- 			require("maven-test.functions").run_all_tests_debug()
--- 		end, { desc = "Run all tests (debug mode)" })
---
--- 		-- Management commands
--- 		vim.api.nvim_buf_create_user_command(args.buf, "MavenTestRestoreCommandsStore", function()
--- 			ensure_loaded()
--- 			require("maven-test.functions").restore_commands_store()
--- 		end, { desc = "Restore stored commands" })
---
--- 		vim.api.nvim_buf_create_user_command(args.buf, "MavenTestCommands", function()
--- 			ensure_loaded()
--- 			require("maven-test.functions").commands()
--- 		end, { desc = "Show commands" })
---
--- 		-- Management arguments
--- 		vim.api.nvim_buf_create_user_command(args.buf, "MavenTestCustomArguments", function()
--- 			ensure_loaded()
--- 			require("maven-test.functions").show_custom_arguments()
--- 		end, { desc = "Show custom arguments" })
---
--- 		vim.api.nvim_buf_create_user_command(args.buf, "MavenTestRestoreArgumentStore", function()
--- 			ensure_loaded()
--- 			require("maven-test.functions").restore_arguments_store()
--- 		end, { desc = "Restore stored commands" })
--- 	end,
--- })
---
---
